7584a8e999de37c6a170341dcd25dbb939d43752
Poorple <91628099+Poorple@users.noreply.github.com>
2023-06-10 20:08:39 +0200

    spring cleaning of main

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,13 @@
 import "./search-bar-style.css";
-import { useState, useMemo, useEffect } from "react";
-import axios from "axios";
 import PokemonCardList from "./components/PokemonListCard";
+import FetchAndFilter from "./FetchAndFilter";
 
 function App() {
-  interface Pkmn {
-    id: number;
-    name: string;
-    type: string[];
-    hp: number;
-    attack: number;
-    defense: number;
-    spattack: number;
-    spdefense: number;
-    speed: number;
-  }
-
-  const [pokemon, setPokemon] = useState<Pkmn[]>([]);
-  const fetchData = async () => {
-    try {
-      const url =
-        "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json";
-      const response = await axios.get(url);
-      const jsonData = response.data;
-
-      const fetchedPokemon: Pkmn[] = jsonData.map((item: any) => ({
-        id: item.id,
-        name: item.name.english,
-        type: item.type,
-        hp: item.base[0],
-        attack: item.base[1],
-        defense: item.base[2],
-        spattack: item.base[3],
-        spdefense: item.base[4],
-        speed: item.base[5],
-      }));
-
-      setPokemon(fetchedPokemon);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-    console.log(pokemon);
-  }, []);
-  const [query, setQuery] = useState("");
-
-  const filteredItems = useMemo(() => {
-    return pokemon.filter((item) => {
-      return item.name.toLowerCase().includes(query.toLowerCase());
-    });
-  }, [pokemon, query]);
-  console.log(filteredItems);
   return (
     <>
       <div>
         <h1>PoKeDex</h1>
-        <input
-          className="search-input"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          type="search"
-          placeholder="Search.."
-        />
-        <h3>Pokemon:</h3>
-
-        <PokemonCardList
-          filteredItems={query.length >= 3 ? filteredItems : []}
-        />
+        <FetchAndFilter />
       </div>
     </>
   );
---
+++ src/FetchAndFilter.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useState, useMemo } from "react";
+import axios from "axios";
+import React from "react";
+import PokemonCardList from "./components/PokemonListCard";
+
+const FetchAndFilter = () => {
+  interface Pkmn {
+    id: number;
+    name: string;
+    type: string[];
+    hp: number;
+    attack: number;
+    defense: number;
+    spattack: number;
+    spdefense: number;
+    speed: number;
+  }
+
+  const [pokemon, setPokemon] = useState<Pkmn[]>([]);
+  const fetchData = async () => {
+    try {
+      const url =
+        "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json";
+      const response = await axios.get(url);
+      const jsonData = response.data;
+
+      const fetchedPokemon: Pkmn[] = jsonData.map((item: any) => ({
+        id: item.id,
+        name: item.name.english,
+        type: item.type,
+        hp: item.base[0],
+        attack: item.base[1],
+        defense: item.base[2],
+        spattack: item.base[3],
+        spdefense: item.base[4],
+        speed: item.base[5],
+      }));
+
+      setPokemon(fetchedPokemon);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+    console.log(pokemon);
+  }, []);
+  const [query, setQuery] = useState("");
+
+  const filteredItems = useMemo(() => {
+    return pokemon.filter((item) => {
+      return item.name.toLowerCase().includes(query.toLowerCase());
+    });
+  }, [pokemon, query]);
+  console.log(filteredItems);
+
+  return (
+    <>
+      <input
+        className="search-input"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="search"
+        placeholder="Search.."
+      />
+      <PokemonCardList filteredItems={query.length >= 3 ? filteredItems : []} />
+    </>
+  );
+};
+
+export default FetchAndFilter;
